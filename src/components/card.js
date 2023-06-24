8538e1e3f3e56735cc05fec9b04201dc47167194
yelisetty-suresh-babu <yelisettysuresh2@gmail.com>
2023-06-24 21:52:06 +0530

    made changes in card.js

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,7 @@ function Card() {
         <h2 class="sr-only">Products</h2>
 
         <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          <a href="#" class="group">
+          <a href="#" className="group">
             <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
               <img
                 src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
